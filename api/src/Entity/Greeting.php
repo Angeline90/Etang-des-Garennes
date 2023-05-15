b4cb3de63ebe244c5bbbf9432659c00f7be602be
Angeline90 <angeline.gauron@gmail.com>
2023-05-15 23:49:26 +0200

    config api platform

--- api/src/Entity/Greeting.php
+++ api/src/Entity/Greeting.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -10,6 +12,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * This is a dummy entity. Remove it!
  */
 #[ApiResource(mercure: true)]
+#[ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'name' => 'ipartial'])]
 #[ORM\Entity]
 class Greeting
 {
