38c71c34293d39955b6ba24dd143dc818bcfe61d
CaroleGG <caroguerite@hotmail.com>
2023-05-25 12:12:13 +0200

    admin section

---
+++ api/src/Controller/AdminController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route(path: '/app', name: 'app_')]
+class AdminController extends AbstractController
+{
+    #[
+        Route('/admin', name: 'admin'),
+        IsGranted('ROLE_ADMIN')
+        ]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+    // #[Route('/user', name: 'user', methods: ['GET'])]
+    // public function getUsers(UserRepository $userRepository): Response
+    // {
+    //     return $this->render('user/index.html.twig', [
+    //         'users' => $userRepository->findAll(),
+    //     ]);
+       
+    // }
+   
+}
