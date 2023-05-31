3514365f122fc626312de6b1582bbb51b1597f7b
CaroleGG <caroguerite@hotmail.com>
2023-05-31 14:37:32 +0200

    crud booking

--- api/src/Controller/BookingController.php
+++ api/src/Controller/BookingController.php
@@ -2,20 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
+use App\Form\BookingType;
+use App\Repository\BookingRepository;
+use App\Repository\CottageRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[
-    Route('/app/booking', name: 'app_')
-    ]
+#[Route(path: '/app/booking', name: 'app_booking_')]
 class BookingController extends AbstractController
 {
-    #[Route('/', name: 'booking')]
-    public function index(): Response
+    #[Route('/', name: 'index', methods: ['GET'])]
+    public function index(BookingRepository $bookingRepository): Response
     {
         return $this->render('booking/index.html.twig', [
-            'controller_name' => 'BookingController',
+            'bookings' => $bookingRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
+    public function new(Request $request, BookingRepository $bookingRepository,UserRepository $userRepository, CottageRepository $cottageRepository): Response
+    {
+        $booking = new Booking();
+        $users =[$this->getUser()];
+        $form = $this->createForm(BookingType::class, $booking,['users' => $users]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bookingRepository->save($booking, true);
+
+            return $this->redirectToRoute('app_booking_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('booking/new.html.twig', [
+            'booking' => $booking,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(Booking $booking): Response
+    {
+        return $this->render('booking/show.html.twig', [
+            'booking' => $booking,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Booking $booking, BookingRepository $bookingRepository): Response
+    {
+        $form = $this->createForm(BookingType::class, $booking);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $bookingRepository->save($booking, true);
+
+            return $this->redirectToRoute('app_booking_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('booking/edit.html.twig', [
+            'booking' => $booking,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
+    public function delete(Request $request, Booking $booking, BookingRepository $bookingRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$booking->getId(), $request->request->get('_token'))) {
+            $bookingRepository->remove($booking, true);
+        }
+
+        return $this->redirectToRoute('app_booking_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- api/src/Form/RegistrationFormType.php
+++ api/src/Form/RegistrationFormType.php
@@ -24,35 +24,10 @@ class RegistrationFormType extends AbstractType
         ->add('name', TextType::class)
         ->add('firstname', TextType::class)
         ->add('email', EmailType::class)
-            // ->add('agreeTerms', CheckboxType::class, [
-            //     'mapped' => false,
-            //     'constraints' => [
-            //         new IsTrue([
-            //             'message' => 'You should agree to our terms.',
-            //         ]),
-            //     ],
-            // ])
-            ->add('password', RepeatedType::class, [
+        ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class])
-                
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                // 'mapped' => false,
-                
-                // 'attr' => ['autocomplete' => 'new-password'],
-                
-                // 'constraints' => [
-                //     new Length([
-                //         'min' => 6,
-                //         'minMessage' => 'Le mot de passe doit être de  {{ limit }} caractères minimum',
-                //         // max length allowed by Symfony for security reasons
-                //         'max' => 20,
-                //         'maxMessage' => 'Le mot de passe doit être de  {{ limit }} caractères maximum',
-                //     ]),
-                // ],
-          
-            ->add('phone', TextType::class)
-            ->add('adress', TextareaType::class) 
+        ->add('phone', TextType::class)
+        ->add('adress', TextareaType::class) 
             
         ;
     }
