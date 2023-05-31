f9df0c50a32a258826cc7081eabc5ed2ac7cd68b
Marina <masamana33@gmail.com>
2023-05-31 14:41:19 +0200

    install vich_upload

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -17,4 +17,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
