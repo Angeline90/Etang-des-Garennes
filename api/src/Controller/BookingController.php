ce13d4e4e52dd22f0a4e7f68e6c4d181c9503365
CaroleGG <caroguerite@hotmail.com>
2023-06-01 10:53:55 +0200

    change index booking template

--- api/src/Controller/BookingController.php
+++ api/src/Controller/BookingController.php
@@ -7,14 +7,14 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[
-    Route('/app/booking', name: 'app_')
+    Route('/app/booking', name: 'app_booking_')
     ]
 class BookingController extends AbstractController
 {
-    #[Route('/', name: 'booking')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('booking/index.html.twig', [
+        return $this->render('booking/_index.html.twig', [
             'controller_name' => 'BookingController',
         ]);
     }
